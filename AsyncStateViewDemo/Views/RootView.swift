e154dfadb1ec98e27b4b2caea2151413282fd02f
Pierluigi Cifani <pcifani@theleftbit.com>
2023-07-20 15:56:11 +0000

    Cleanup

---
+++ AsyncStateViewDemo/Views/RootView.swift
@@ -0,0 +1,21 @@
+
+import SwiftUI
+
+struct RootView: View {
+  var body: some View {
+    AsyncStateView(id: "root-view") {
+      try await RootView.fetchTabs()
+    } hostedViewGenerator: {
+      ContentView(tabs: $0)
+    } errorViewGenerator: { error, _ in
+      Text("Error Loading Initial Tabs")
+    } loadingViewGenerator: {
+      ProgressView()
+    }
+  }
+  
+  static func fetchTabs() async throws -> [Tab] {
+    try await Task.sleep(for: .seconds(1))
+    return Tab.mockTabs()
+  }
+}
