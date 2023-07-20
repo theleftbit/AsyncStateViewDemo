40980f4688a76fa521c4adb6bc79b278552eb5fe
Pierluigi Cifani <pcifani@theleftbit.com>
2023-07-20 12:08:41 +0000

    Initial Commit

---
+++ AsyncStateViewDemo/AsyncStateViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncStateViewDemoApp.swift
+//  AsyncStateViewDemo
+//
+//  Created by Pierluigi Cifani on 20/7/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncStateViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
